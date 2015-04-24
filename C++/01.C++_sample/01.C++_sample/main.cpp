790cfe5ab0e64ca5e16fec72317bebe90f5078a8
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-04-24 09:04:04 +0900

    once add app file and commit

---
+++ C++/01.C++_sample/01.C++_sample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  01.C++_sample
+//
+//  Created by yhiki on 2015/04/14.
+//  Copyright (c) 2015年 C++. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
